64d14a93b141307b7ca9c8bb1a5fea15e040c580
shady00s <ssk2310@hotmail.com>
2023-03-20 21:03:02 +0200

    edited server -5

--- ayno-chat/src/axios/inestance.jsx
+++ ayno-chat/src/axios/inestance.jsx
@@ -7,7 +7,8 @@ const axiosInestance = axios.create({
     baseURL:"https://ayno-chat-api.onrender.com/",
     withCredentials:true
 })
-
+//"https://ayno-chat-api.onrender.com
+//https://ayno-chat-api.onrender.com
 
 // const axiosUserInestance = axios.create({
 //     baseURL:"http://192.168.1.4:8080/user",
