604dec717e37027e9b734ec2f90b71a085b1a7f2
shady00s <ssk2310@hotmail.com>
2023-03-20 19:54:39 +0200

    edited server -4

--- server-side/src/server.ts
+++ server-side/src/server.ts
@@ -39,7 +39,7 @@ export const store = new MongoDBStore({
 
 
 app.use('/',(req:Request,res:Response,next:NextFunction)=>{
-    res.setHeader('Access-Control-Allow-Origin','http://192.168.1.4:3000')
+    res.setHeader('Access-Control-Allow-Origin','https://ayno-chat.vercel.app')
     res.setHeader(
         'Access-Control-Allow-Methods',
         'OPTIONS, GET, POST, PUT, PATCH, DELETE'
