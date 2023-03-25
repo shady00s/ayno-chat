1a200b289205ba242b2c33cd113bc900c8b344be
shady00s <ssk2310@hotmail.com>
2023-03-25 13:52:28 +0200

    client-side error fixes

--- ayno-chat/src/home/components/friend_request/friend_request_component.jsx
+++ ayno-chat/src/home/components/friend_request/friend_request_component.jsx
@@ -19,15 +19,12 @@ export function FriendRequestComponent() {
    
   
 
-    useEffect(()=>{
-        setNumberOfReq(friendRequest.length)
-
-    },[friendRequest])
+ 
 
     useEffect(()=>{
         setFriendRequest(()=>[...friendRequest,...notifications.friendRequestNotification])
 
-    },[notifications.friendRequestNotification])
+    },[notifications])
 
     useEffect(() => {
             setLoading(true)
@@ -41,13 +38,17 @@ export function FriendRequestComponent() {
             })
 
     }, [])
+    useEffect(()=>{
+        setNumberOfReq(friendRequest.length)
+
+    },[friendRequest])
     return (
         <>
             <div className="pt-4 pd-4">
                 <div onClick={()=>{setOpen(!open)}} className="flex justify-between pl-3 pt-2 pb-2 pr-3 cursor-pointer hover:bg-[rgba(124,173,219,0.04)]">
                     <h1 className="text-slate-200 select-none">Friend Requests</h1>
                     <div className="flex justify-evenly w-[8rem]">
-                    <div ><span className="text-sm text-slate-400">{friendRequest.length} requests</span></div>
+                    <div ><span className="text-sm text-slate-400">{numberofReq} requests</span></div>
                     <ChevronDown className="text-slate-200" />
                     </div>
                     
@@ -57,6 +58,11 @@ export function FriendRequestComponent() {
                     {loading?<FriendListSkeleton/>:friendRequest.length !==0?friendRequest.map(data=><FriendRequestBody key={data.name} data={data} removeFriendRequest={(removedId)=>{
                         const requests = friendRequest.filter(id=> id.name !== removedId.name )
                       setFriendRequest(requests)
+                      let editedNotifications = notifications
+
+                      editedNotifications.friendRequestNotification = requests
+
+                      setNotifications(editedNotifications)
                     }}/> ):<div className="flex flex-col w-full h-full justify-center items-center">
                                 <CloudRain size={44} className="p-1 stroke-slate-600 "/>
                             <h1 className="text-slate-400">There is no friend requests found</h1>
