87d003befdea37ed92bf02640512abbe51f3a3fa
shady00s <ssk2310@hotmail.com>
2023-03-25 13:55:56 +0200

    client-side error fixes

--- ayno-chat/src/home/components/friend_request/friend_request_component.jsx
+++ ayno-chat/src/home/components/friend_request/friend_request_component.jsx
@@ -24,7 +24,7 @@ export function FriendRequestComponent() {
     useEffect(()=>{
         setFriendRequest(()=>[...friendRequest,...notifications.friendRequestNotification])
 
-    },[notifications])
+    },[notifications.friendRequestNotification])
 
     useEffect(() => {
             setLoading(true)
