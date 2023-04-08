313a51eaad0d25101cf789d441448c6a4a1f8e5c
shady00s <ssk2310@hotmail.com>
2023-04-08 13:52:12 +0200

    socket fixing

--- ayno-chat/src/axios/inestance.jsx
+++ ayno-chat/src/axios/inestance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInestance = axios.create({
-    baseURL:"https://ayno-chat-api.onrender.com/",
+    baseURL:"http://192.168.1.4/",
     withCredentials:true
 })
 //"https://ayno-chat-api.onrender.com
@@ -9,7 +9,7 @@ const axiosInestance = axios.create({
 
 
 const userInstance = axios.create({
-    baseURL:"https://ayno-chat-api.onrender.com/user",
+    baseURL:"https://192.168.1.4/user",
     withCredentials:true
 })
 
